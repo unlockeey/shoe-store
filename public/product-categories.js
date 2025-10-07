238c3d8c72e59d7d132eeaf04553a1391bd902b9
unlockeey <eddymuchiry04@gmail.com>
2025-10-07 21:02:42 +0300

    Create product-categories.js

---
+++ public/product-categories.js
@@ -0,0 +1,10 @@
+const categories = {
+  running: { name: "Running Shoes", brands: ["Nike", "Adidas", "New Balance"] },
+  casual: { name: "Casual Sneakers", brands: ["Nike", "Adidas", "Converse", "Vans"] },
+  basketball: { name: "Basketball Shoes", brands: ["Nike", "Jordan", "Adidas"] },
+  football: { name: "Football Boots", brands: ["Nike", "Adidas", "Puma"] },
+  sandals: { name: "Sandals", brands: ["Adidas", "Nike", "Puma"] },
+  formal: { name: "Formal Shoes", brands: ["Bata", "Hush Puppies"] }
+};
+
+module.exports = categories;
